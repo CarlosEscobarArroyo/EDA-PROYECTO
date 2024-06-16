23facbbed736e83662adedb07972e5ccef07d6ff
Pengui010 <20223087@aloe.ulima.edu.pe>
2024-06-15 22:05:57 -0500

    Add files via upload

---
+++ src/modelos/Interesado.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Alonso
+ */
+public class Interesado extends Usuario{
+    
+    public Interesado(String nombre, String apellido, Integer edad, Integer dni) {
+        super(nombre, apellido, edad, dni);
+    }
+    
+}
