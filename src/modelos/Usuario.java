bc0d408979dfb220e6e612ecd574ed79cac090c9
Pengui010 <20223087@aloe.ulima.edu.pe>
2024-06-15 22:03:04 -0500

    Update Usuario.java

--- src/modelos/Usuario.java
+++ src/modelos/Usuario.java
@@ -20,6 +20,37 @@ public class Usuario {
         this.edad = edad;
         this.dni = dni;
     }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
     
     
  
