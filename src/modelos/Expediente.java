e23fc4e0f08ec9432be1ceaeff3bf2c1b7961da3
n04613 <n04613@N04613.lab.aca>
2024-06-17 16:45:30 -0500

    expediente

---
+++ src/modelos/Expediente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author n04613
+ */
+public class Expediente {
+    private int numExpediente;
+    private Prioridad prioridad;
+    private String Documento;
+    private Usuario user;
+    
+}
