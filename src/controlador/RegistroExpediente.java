f5695520273f982fe178c67a9bd6851c186b7d7b
n04130 <n04130@N04130.lab.aca>
2024-06-18 09:55:15 -0500

    registroExpediente

--- src/controlador/RegistroExpediente.java
+++ src/controlador/RegistroExpediente.java
@@ -1,10 +1,21 @@
 
 package controlador;
-
+import tda.*;
+import modelos.*;
 /**
  *
  * @author Alonso
  */
 public class RegistroExpediente {
+    Cola<Expediente> expedientes;
+    
+    RegistroExpediente(){
+        this.expedientes= new Cola<>();
+        Interesado interesado=new Interesado("Alonso", "Almerc", 20, 12312);
+        Expediente expediente1=new Expediente(0, "alta", "no sé", interesado);
+
+    }
+    
+    
     
 }
