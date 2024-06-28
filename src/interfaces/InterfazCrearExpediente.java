9db3d52fad67d3f02a7bcb6007be6122515fffbb
Pengui010 <20223087@aloe.ulima.edu.pe>
2024-06-28 17:13:25 -0500

    Update InterfazCrearExpediente.java

--- src/interfaces/InterfazCrearExpediente.java
+++ src/interfaces/InterfazCrearExpediente.java
@@ -267,7 +267,7 @@ public class InterfazCrearExpediente extends javax.swing.JFrame {
             String dependenciaNombre = (String)jComboBox1.getSelectedItem();
             String prioridad1 = (String)txtPrioridad.getSelectedItem();
 
-            Interesado interesado = new Interesado(nombre, dni, email);        
+            Interesado interesado = new Interesado(nombre, dni, telefono, email);        
             Prioridad prioridad = new Prioridad(prioridad1);
             Expediente expediente = new Expediente(prioridad, informacion, interesado, dependenciaNombre);
             
